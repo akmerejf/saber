6bad491765394506801f0f1c17f3f2b1ec68cb8f
akmerejf <akmerejf@gmail.com>
2013-11-04 11:33:44 -0300

    projeto iniciado

---
+++ revistaSaber/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RevistaSaber::Application.config.secret_key_base = '1e3bc0af13bf97106f0ca232ac2cbde8000b60abd1161f1e4d422ab7bc51d5b83aa33c75f6df2946032a7538a0d7e980718f53f2ff0b78da1d4cbec4cbe2d6b7'
